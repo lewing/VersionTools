8d4f571592991c79500ae2378f58ca8d77e85683
Larry Ewing <lewing@microsoft.com>
2019-10-23 18:44:05 -0500

    Initial commit

---
+++ EngUpdater/Program.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Xml;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace EngUpdater
+{
+    public class VersionDetails {
+        public string Name;
+        public string Version;
+        public string Uri;
+        public string Sha;
+        public string CoherentParentDependency;
+    }
+
+    class Program {
+        static async Task Main(string[] args)
+        {
+            await UpdateXml (args.Length > 0 ? args[0] : null);
+        }
+
+        public static async Task UpdateXml (string path)
+        {
+            var versionsSourceStream = await GitHub.GetRaw ("dotnet/toolset", "release/3.1.1xx", "eng/Versions.props");
+            var versionsTargetStream = await GitHub.GetRaw ("mono/msbuild", "mono-2019-08", "eng/Versions.props");
+            var detailsSourceStream = await GitHub.GetRaw ("dotnet/toolset", "release/3.1.1xx", "eng/Version.Details.xml");
+            var packagesTargetStream = await GitHub.GetRaw ("mono/msbuild", "mono-2019-08", "eng/Packages.props");
+            var details = await ReadVersionDetails (detailsSourceStream);
+            var detailsTargetStream = await GitHub.GetRaw ("mono/msbuild", "mono-2019-08", "eng/Version.Details.xml");
+            var versions = await ReadCurrent (versionsSourceStream);
+            versions.Remove ("PackageVersionPrefix");
+            versions.Remove ("VersionPrefix");
+
+            foreach (var detail in details.Values) {
+                var vkey = $"{detail.Name.Replace (".", "")}PackageVersion";
+
+                if (versions.TryGetValue (vkey, out var version)) {
+                    Console.WriteLine ($"{vkey}: {version} ====> {detail.Version} - {detail.Uri} - {detail.Sha}");
+                } else {
+                    Console.WriteLine ($"No match for {vkey}");
+                }
+            }
+            
+            Stream detailsOutputStream = null;
+            Stream versionsOutputStream = null;
+            Stream packagesOutputStream = null;
+            if (path != null) {
+                detailsOutputStream = File.Create (Path.Combine (path, "eng/Version.Details.xml"));
+                versionsOutputStream = File.Create (Path.Combine (path, "eng/Versions.props"));
+                packagesOutputStream = File.Create (Path.Combine (path, "eng/Packages.props"));
+            }
+
+
+            await UpdateProps (versionsTargetStream, versionsOutputStream, versions);
+            await UpdateDetails (detailsTargetStream, detailsOutputStream, details);
+            await UpdateProps (packagesTargetStream, packagesOutputStream, versions);
+        }
+
+        public static async Task<Dictionary<string,VersionDetails>> ReadVersionDetails (Stream stream)
+        {
+            var versions = new Dictionary<string,VersionDetails> ();
+            using (XmlReader reader = XmlReader.Create(stream, new XmlReaderSettings () { Async = true }))
+            {
+                void ReadDetails (XmlReader read, VersionDetails detail) 
+                {
+                        var name = read.Name;
+                        if (read.NodeType == XmlNodeType.Element)
+                            read.Read ();
+
+                        switch (name) {
+                            case "Name":
+                                detail.Name = read.Value;
+                                break;
+                            case nameof (VersionDetails.Version):
+                                detail.Version = read.Value;
+                                break;
+                            case nameof (VersionDetails.Uri):
+                                detail.Uri = read.Value;
+                                break;
+                            case nameof (VersionDetails.Sha):
+                                detail.Sha = read.Value;
+                                break;
+                            case nameof (VersionDetails.CoherentParentDependency):
+                                detail.CoherentParentDependency = read.Value;
+                                break;
+                            default:
+                                read.Skip ();
+                                break;
+                        }
+                }
+
+                VersionDetails details = null;
+                while (await reader.ReadAsync()) {
+                    switch (reader.NodeType) {
+                    case XmlNodeType.Element:
+                        switch (reader.Name) {
+                            case "Dependency":
+                                details = new VersionDetails ();
+                                while (reader.MoveToNextAttribute()) {
+                                    ReadDetails (reader, details);
+                                }
+                                break;
+                            case "ProductDependencies":
+                            case "Dependencies":
+                                //Console.WriteLine("Start Element {0}", reader.Name);
+                                break;
+                            default:
+                                if (details != null) {
+                                    ReadDetails (reader, details);
+                                }
+                                break;
+                        }
+                        break;
+                    case XmlNodeType.EndElement:
+                        //Console.WriteLine("End Element {0}", reader.Name);
+                        if (reader.Name == "Dependency") {
+                            if (details != null) {
+                                versions [details.Name] = details;
+                                Console.WriteLine ($"{details.Name} = {details.Version}");
+                            }
+                            details = null;
+                        }
+                        break;
+                    default:
+                        break;
+                    }
+                }
+                return versions;
+            }
+        }
+
+        static async Task<Dictionary<string,string>> ReadCurrent (Stream stream)
+        {
+            using (XmlReader reader = XmlReader.Create(stream, new XmlReaderSettings () { Async = true }))
+            {
+                var settings = new Dictionary<string,string>();
+                bool inGroup = false;
+                while (await reader.ReadAsync()) {
+                    switch (reader.NodeType) {
+                    case XmlNodeType.Element:
+                        switch (reader.Name) {
+                            case string key when inGroup:
+                                if (await reader.ReadAsync ()) {
+                                    var value = await reader.GetValueAsync ();
+                                    //Console.WriteLine($"{key} = {value}");
+                                    if (key.Contains ("Version") && Char.IsDigit (value[0])) {
+                                        var packageKey = !key.EndsWith ("PackageVersion") ? key.Replace ("Version", "PackageVersion") : key;
+                                        settings[packageKey] = value;
+                                        Console.WriteLine($"Stored {packageKey} = {value}");
+                                    }
+                                } else {
+                                    Console.WriteLine ("odd");
+                                }
+                                break;
+                            case "PropertyGroup":
+                                inGroup = true;
+                                break;
+                            case "Project":
+                                //Console.WriteLine("Start Element {0}", reader.Name);
+                                break;
+                            default:
+                                reader.Skip();
+                                break;
+                        }
+                        break;
+                    case XmlNodeType.EndElement:
+                        if (reader.Name == "PropertyGroup")
+                            inGroup = false;
+                        break;
+                    default:
+                        break;
+                    }
+                }
+                return settings;
+            }
+        }
+
+        public static async Task UpdateProps (Stream inputStream, Stream outputStream, Dictionary<string,string> versions)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            //settings.Indent = true;
+            //settings.OmitXmlDeclaration = true;
+            settings.Encoding = new UTF8Encoding(false);
+            settings.Indent = true;
+            using (var reader = XmlReader.Create (inputStream, new XmlReaderSettings { Async = true })) {
+                using (var writer = outputStream != null ? XmlWriter.Create (outputStream, settings) : XmlWriter.Create (Console.Out)) {
+                    while (await reader.ReadAsync()) {
+                        var name = reader.Name;//reader.Name.Replace ("PackageVersion", "Version");
+                        if (reader.NodeType == XmlNodeType.Element && versions.TryGetValue (name, out var value)) {
+                            writer.WriteElementString(reader.Name, value);
+                            reader.Skip ();
+                        } else {
+                            writer.WriteNode (reader);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static async Task UpdateDetails (Stream inputStream, Stream outputStream, Dictionary<string,VersionDetails> versions)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.IndentChars = "  ";
+            //settings.OmitXmlDeclaration = true;
+            settings.Encoding = new UTF8Encoding(false);
+            using (var reader = XmlReader.Create (inputStream, new XmlReaderSettings { Async = true })) {
+                using (var writer = outputStream != null ? XmlWriter.Create (outputStream, settings) : XmlWriter.Create (Console.Out)) {
+                    while (await reader.ReadAsync ()) {
+                        if (reader.NodeType == XmlNodeType.Element && reader.Name == "Dependency") {
+                            var name = reader.GetAttribute ("Name");
+                            if (versions.TryGetValue (name, out var details)) {
+                                writer.WriteDependency (reader, details);
+                            } else {
+                                writer.WriteNode (reader);
+                            }
+                        } else {
+                            writer.WriteNode (reader);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static class XmlIOExtensions {
+        public static void WriteDependency (this XmlWriter writer, XmlReader reader, VersionDetails details)
+        {
+                writer.WriteStartElement (reader.Prefix, reader.LocalName, reader.NamespaceURI);
+                writer.WriteAttributeString ("Name", reader.NamespaceURI, details.Name);
+                writer.WriteAttributeString ("Version", reader.NamespaceURI, details.Version);
+                if (details.CoherentParentDependency != null)
+                    writer.WriteAttributeString ("CoherentParentDependency", reader.NamespaceURI, details.CoherentParentDependency);
+                
+                while (reader.Read()) {
+                    switch (reader.NodeType) {
+                    case XmlNodeType.Element:
+                        if (reader.Name == "Uri") {
+                            writer.WriteNode (reader);
+                            writer.WriteString(details.Uri);
+                            reader.Read();
+                            reader.Read();
+                            writer.WriteNode (reader);
+                        } else if (reader.Name == "Sha") {
+                            writer.WriteNode (reader);
+                            writer.WriteString(details.Sha);
+                            reader.Read();
+                            reader.Read();
+                            writer.WriteNode (reader);
+                        } else {
+                            writer.WriteNode (reader);
+                        }
+                        break;
+                    case XmlNodeType.EndElement:
+                        writer.WriteNode (reader);
+                        if (reader.Name == "Dependency")
+                            return;
+                        
+                        break;
+                    default:
+                        writer.WriteNode (reader);
+                        break;
+                    }
+                
+            }
+        }
+
+        public static void WriteNode (this XmlWriter writer, XmlReader reader)
+        {
+            switch (reader.NodeType) {
+            case XmlNodeType.Element:
+                writer.WriteStartElement (reader.Prefix, reader.LocalName, reader.NamespaceURI);
+                writer.WriteAttributes (reader, true );
+
+                if (reader.IsEmptyElement)
+                    writer.WriteEndElement();
+
+                break;
+            case XmlNodeType.EndElement:
+                writer.WriteFullEndElement ();
+                break;
+            case XmlNodeType.Text:
+                writer.WriteString (reader.Value);
+                break;
+            case XmlNodeType.Whitespace:
+            case XmlNodeType.SignificantWhitespace:
+                writer.WriteWhitespace (reader.Value);
+                break;
+            case XmlNodeType.CDATA:
+                writer.WriteCData (reader.Value);
+                break;
+            case XmlNodeType.EntityReference:
+                writer.WriteEntityRef (reader.Name);
+                break;
+            case XmlNodeType.XmlDeclaration:
+            case XmlNodeType.ProcessingInstruction:
+                writer.WriteProcessingInstruction (reader.Name, reader.Value);
+                break;
+            case XmlNodeType.DocumentType:
+                writer.WriteDocType (reader.Name, reader.GetAttribute("PUBLIC"), reader.GetAttribute("SYSTEM"), reader.Value);
+                break;
+            case XmlNodeType.Comment:
+                writer.WriteComment (reader.Value);
+                break;
+            }
+        }
+    }
+
+    class GitHub {
+        static HttpClient client = new HttpClient();
+
+        public static Task<Stream> GetRaw (string repo, string version, string path)
+        {
+            var location = new Uri($"https://raw.githubusercontent.com/{repo}/{version}/{path}");
+            return client.GetStreamAsync(location);
+        }
+
+        public static async Task<(string sha, DateTimeOffset date) []> GetCommits(string path, string repo)
+        {
+            try {
+                client.DefaultRequestHeaders.Add("Accept", "*/*");
+                client.DefaultRequestHeaders.Add("User-Agent", "curl/7.54.0");
+
+                var data = await client.GetStringAsync ( new Uri ($"https://api.github.com/repos/{repo}/commits?path={path}"));
+                var obj = new [] {
+                    new {
+                        sha = "",
+                        commit = new {
+                            author = new {
+                                date = DateTimeOffset.Now
+                            }
+                        }
+                    }
+                };
+
+                return JsonConvert.DeserializeAnonymousType (data, obj).Select (o => ValueTuple.Create (o.sha, o.commit.author.date)).ToArray();
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+                return Array.Empty<(string sha, DateTimeOffset date)>();
+            }
+        }
+    }
+}
---
+++ InsertMonoAddin/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Xml;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Text.Json;
+
+/* 
+[
+    {
+        "url": "https://xamjenkinsartifact.azureedge.net/build-package-osx-mono/2019-08/137/1b2e536b2238aa6bd86722996bcd1cf0f76cd6f3/MonoFramework-MDK-6.6.0.140.macos10.xamarin.universal.pkg",
+        "sha256": "26668943d1c32f09582a73ee8d5e4274dfa8d2aa936520e7f2c4cdaf76a963d6",
+        "md5": "bd8df61072a018dfd4d0c72ae5b62349",
+        "size": 357293878,
+        "productId": "964ebddd-1ffe-47e7-8128-5ce17ffffb05",
+        "releaseId": "606000140",
+        "version": "6.6.0.140"
+    }
+]
+*/
+
+/*
+{
+    "url": "https://xamjenkinsartifact.azureedge.net/build-package-osx-mono/2019-06/177/7da9a041b3b69d2f6ac04f8c2b2a1814d4d468f4/MonoFramework-MDK-6.4.0.194.macos10.xamarin.universal.pkg",
+    "version": "6.4.0.194",
+    "url": "https://xamjenkinsartifact.azureedge.net/build-package-osx-mono/2019-06/180/fe64a4765e6d1dbb41d5c86708fcb02aa519247a/MonoFramework-MDK-6.4.0.198.macos10.xamarin.universal.pkg",
+    "version": "6.4.0.198",
+    "uploaded": true,
+    "repo": "git@github.com:mono/mono",
+    "commit": "7da9a041b3b69d2f6ac04f8c2b2a1814d4d468f4",
+    "tag": "",
+    "productId": "964ebddd-1ffe-47e7-8128-5ce17ffffb05",
+    "releaseId": "604000194",
+    "sha256": "12834b45db0050b55556291f59f76bb1c0e2b37d336d57eb19c1cd0db147692a",
+    "md5": "6efd44319a4fc25dda16b9e6bdf07e8e",
+    "size": 353257385,
+    "releaseId": "604000198",
+    "sha256": "07f3622e5ec47ed000c9668702d45acf19f49602c10b0aaa055cc7e454cdc695",
+    "md5": "97c876c536dc5049265577cb4d0f902b",
+    "size": 352894286,
+}
+ */
+
+namespace InsertMonoAddin
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var artifactsUri = args [0];
+            string mdAddinsPath = null; //args [1];
+
+            await UpdateMdAddins (artifactsUri, mdAddinsPath);
+        }
+
+        public static async Task UpdateMdAddins (string artifactsUri, string mdAddinsPath) {
+            var artifactsStream = File.OpenRead (artifactsUri);
+            var artifact = (await JsonSerializer.DeserializeAsync<Artifacts[]>(artifactsStream))[0];
+            
+            var parts = artifact.url.Split (new [] {'/'});
+            var commit = parts[parts.Length - 2];
+            Console.WriteLine ($"commit = {commit}");
+            var mono = new MonoExternal (){
+                url = artifact.url,
+                version = artifact.version,
+                productId = artifact.productId,
+                releaseId = artifact.releaseId,
+                sha256 = artifact.sha256,
+                md5 = artifact.md5,
+                size = artifact.size
+            };
+
+            Console.WriteLine (JsonSerializer.Serialize(mono, new JsonSerializerOptions () { WriteIndented = true }));
+        }
+    }
+
+    public class Artifacts {
+        public string url { get; set; }
+        public string sha256 { get; set; }
+        public string md5 { get; set; }
+        public long size { get; set; }
+        public string productId { get; set; }
+        public string releaseId { get; set; }
+        public string version { get; set; }
+    }
+    public class MonoExternal {
+        public string url { get; set; }
+        public string version { get; set; }
+        public bool uploaded { get; set; } = true;
+        public string repo { get; set; } = "git@github.com:mono/mono";
+        public string tag { get; set; } = "";
+        public string productId { get; set; }
+        public string releaseId { get; set; }
+        public string sha256 { get; set; }
+        public string md5 { get; set; }
+        public long size { get; set; }
+    }
+}
