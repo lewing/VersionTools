86269806c7009c556ebee81bf2344e8b9a6fc484
Ankit Jain <radical@gmail.com>
2020-01-12 18:34:04 -0500

    GitHub.cs: Refactoring, +AddReviewers(..)

--- EngUpdater/GitHub.cs
+++ EngUpdater/GitHub.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.IO;
 using System.Text;
 using System.Linq;
 using System.Text.Json;
+using System.Diagnostics.CodeAnalysis;
 
 #nullable enable
 
@@ -35,23 +37,22 @@ namespace EngUpdater
             if (String.IsNullOrEmpty (branch))
                 throw new ArgumentNullException (nameof(branch));
 
-            string uriString = $"https://api.github.com/repos/{repo}/git/ref/heads/{branch}";
+            string gh_rest_url = $"/repos/{repo}/git/ref/heads/{branch}";
             try {
-                var shaJsonElement = await GetJsonFromApiRequest (uriString, json_path: "object/sha", verbose: verbose);
-                return shaJsonElement.GetString();
+                return await GetJsonFromApiRequest<string> (gh_rest_url, json_path: "object/sha", verbose: verbose);
+            } catch (KeyNotFoundException knfe) {
+                Console.WriteLine ($"Failed to find relevant fields in the reponse: {knfe.ToString ()}, for uri: {gh_rest_url}.");
             } catch (HttpRequestException hre) {
-                if (verbose) {
-                    Console.WriteLine ($"github request failed with: {hre.ToString ()}, for uri: {uriString}.");
-                }
-
-                return null;
+                Console.WriteLine ($"github request failed with: {hre.ToString ()}, for uri: {gh_rest_url}.");
             }
+
+            return null;
         }
 
-        public static async Task<JsonElement> GetJsonFromApiRequest (string uri_string,
-                                                                     IDictionary<string, string>? additional_request_headers = null,
-                                                                     string? json_path = null,
-                                                                     bool verbose = false)
+        public static async Task<T> GetJsonFromApiRequest<T> (string gh_rest_url,
+                                                              IDictionary<string, string>? additional_request_headers = null,
+                                                              string? json_path = null,
+                                                              bool verbose = false)
         {
             client.DefaultRequestHeaders.Add ("Accept", "*/*");
             client.DefaultRequestHeaders.Add ("User-Agent", "curl/7.54.0");
@@ -61,53 +62,115 @@ namespace EngUpdater
                     client.DefaultRequestHeaders.Add (kvp.Key, kvp.Value);
             }
 
+            var uri_string = $"https://api.github.com{gh_rest_url}";
+            if (verbose)
+                Console.WriteLine ($"GetJsonFromApiRequest {uri_string}");
+
             var stream = await client.GetStreamAsync (new Uri (uri_string));
-            var jdoc = JsonDocument.Parse(stream);
-            if (!string.IsNullOrEmpty (json_path) && JsonHelper.TryGetElementByPath (jdoc.RootElement, json_path, out var resultJsonElement))
-                return resultJsonElement;
-            
-            return jdoc.RootElement;
+            var jdoc = await JsonDocument.ParseAsync(stream);
+            return jdoc.RootElement.Get<T>(json_path);
         }
 
-        public static async Task<(bool result, string html_url, int pr_number)> CreatePullRequest (
-                                                            string target_owner_repo,
-                                                            string base_branch_name,
-                                                            string head_ref,
-                                                            string personal_access_token,
-                                                            string title,
-                                                            string? body = null,
-                                                            bool verbose = false)
+        public static async Task<HttpResponseMessage> PostOrDeleteAPI (HttpMethod method,
+                                                                       string gh_rest_url,
+                                                                       string personal_access_token,
+                                                                       string json_string,
+                                                                       bool verbose = false)
         {
+            if (method != HttpMethod.Post && method != HttpMethod.Delete)
+                throw new ArgumentException($"Only HttpMethod.Post or HttpMethod.Delete are supported. Got {method}");
+
             if (String.IsNullOrEmpty (personal_access_token))
                 throw new ArgumentException (nameof(personal_access_token));
 
             client.DefaultRequestHeaders.Add ("Accept", "*/*");
             client.DefaultRequestHeaders.Add ("User-Agent", "curl/7.54.0");
-            client.DefaultRequestHeaders.Add ("Authorization", $"token {personal_access_token}");
+            if (!client.DefaultRequestHeaders.Contains ("Authorization"))
+                client.DefaultRequestHeaders.Add ("Authorization", $"token {personal_access_token}");
 
+            var uri_string = $"https://api.github.com{gh_rest_url}";
+            if (verbose)
+                Console.WriteLine ($"{method}: {uri_string}");
+
+            if (method == HttpMethod.Post) {
+                var content = new StringContent (json_string, Encoding.UTF8, "application/json");
+                return await client.PostAsync(uri_string, content);
+            } else if (method == HttpMethod.Delete) {
+                return await client.DeleteAsync(uri_string);
+            } else {
+                throw new NotImplementedException(method.ToString ());
+            }
+        }
+
+        public static async Task<(bool result, string html_url, int pr_number)> CreatePullRequest (string target_owner_repo,
+                                                                                                   string base_branch_name,
+                                                                                                   string head_ref,
+                                                                                                   string personal_access_token,
+                                                                                                   string title,
+                                                                                                   string? body = null,
+                                                                                                   string[]? reviewers = null,
+                                                                                                   bool verbose = false)
+        {
             var paramsJson = JsonSerializer.Serialize (new {
-                title = title,
+                title,
                 @base = base_branch_name,
                 head = head_ref,
-                body = body
+                body
                 // draft = true
             });
-            var content = new StringContent(paramsJson, Encoding.UTF8, "application/json");
 
-            string uriString = $"https://api.github.com/repos/{target_owner_repo}/pulls";
-            var response = await client.PostAsync (uriString, content);
+            string uriString = $"/repos/{target_owner_repo}/pulls";
+            var response = await PostOrDeleteAPI (HttpMethod.Post, uriString, personal_access_token, paramsJson, verbose);
 
             if (!response.IsSuccessStatusCode) {
                 Console.WriteLine ($"Error: Failed to create PR: {response.ReasonPhrase} {await response.Content.ReadAsStringAsync ()}, for uri: {uriString}");
             } else {
-                var jdoc = JsonDocument.Parse (await response.Content.ReadAsStringAsync ());
-                if (jdoc.RootElement.TryGetProperty ("html_url", out var urlJsonElement) &&
-                    jdoc.RootElement.TryGetProperty ("number", out var numberJsonElement)) {
-                    return (result: true, html_url: urlJsonElement.GetString (), pr_number: numberJsonElement.GetInt32 ());
+                var je = JsonDocument.Parse (await response.Content.ReadAsStringAsync ())
+                                            .RootElement;
+
+                try
+                {
+                    var html_url = je.Get<string>("html_url");
+                    var pr_number = je.Get<int>("number");
+
+                    if (reviewers != null)
+                        await AddReviewers (target_owner_repo, pr_number, personal_access_token, reviewers, verbose);
+
+                    return (result: true, html_url, pr_number);
+                } catch (HttpRequestException hre) {
+                    if (verbose)
+                        Console.WriteLine ($"Adding reviewers failed: {hre.Message}");
+                } catch (KeyNotFoundException knfe) {
+                    if (verbose)
+                        Console.WriteLine ($"Adding reviewers failed: {knfe.Message}");
                 }
             }
 
             return (result: false, String.Empty, -1);
+        }
+
+        public static async Task<bool> AddReviewers (string target_owner_repo,
+                                                     int pr_number,
+                                                     string personal_access_token,
+                                                     string[] reviewers,
+                                                     bool verbose = false)
+        {
+            var paramsJson = JsonSerializer.Serialize (new {
+                reviewers = reviewers
+            });
+
+            // POST /repos/:owner/:repo/pulls/:pull_number/requested_reviewers
+            string gh_url = $"/repos/{target_owner_repo}/pulls/{pr_number}/requested_reviewers";
+            var response = await PostOrDeleteAPI (HttpMethod.Post, gh_url, personal_access_token, paramsJson, verbose);
+
+            if (response.StatusCode == HttpStatusCode.Created) {
+                if (verbose)
+                    Console.WriteLine ($"Added reviewers.");
+            } else {
+                Console.WriteLine ($"Error: Failed to add reviewers. {response.ReasonPhrase} {await response.Content.ReadAsStringAsync ()}, for uri: {gh_url}");
+            } 
+
+            return response.StatusCode == HttpStatusCode.Created;
         }
 
         public static async Task<(string sha, DateTimeOffset date) []> GetCommits(string repo, string path)
@@ -136,58 +199,51 @@ namespace EngUpdater
             }
         }
 
-        public static async IAsyncEnumerable<PullRequest?> GetPullRequests(string api_uri_string, bool verbose = false)
+        public static async IAsyncEnumerable<PullRequest?> GetPullRequests(string gh_rest_url, bool verbose = false)
         {
-            if (String.IsNullOrEmpty (api_uri_string))
-                throw new ArgumentNullException (nameof(api_uri_string));
+            if (String.IsNullOrEmpty (gh_rest_url))
+                throw new ArgumentNullException (nameof(gh_rest_url));
 
-            var je = await GitHub.GetJsonFromApiRequest (api_uri_string, verbose: verbose);
+            var je = await GitHub.GetJsonFromApiRequest<JsonElement> (gh_rest_url, verbose: verbose);
             if (je.ValueKind == JsonValueKind.Array) {
                 foreach (var pe in je.EnumerateArray ())
-                    yield return ParsePullRequest (pe);
+                    yield return PullRequest.Parse(pe);
             } else {
-                yield return ParsePullRequest (je);
-            }
-
-            PullRequest? ParsePullRequest (JsonElement pe)
-            {
-                if (pe.ValueKind == JsonValueKind.Object &&
-                    pe.TryGetProperty ("html_url", out var htmlUrlJsonElement) &&
-                    pe.TryGetProperty ("number", out var numberJsonElement) &&
-                    JsonHelper.TryGetElementByPath (pe, "head/ref", out var headRefJsonElement) &&
-                    JsonHelper.TryGetElementByPath (pe, "head/repo/owner/login", out var headOwnerJsonElement) &&
-                    JsonHelper.TryGetElementByPath (pe, "base/ref", out var baseRefJsonElement)) {
-                        
-                    return new PullRequest (
-                        HTML_URL            : htmlUrlJsonElement.GetString (),
-                        HeadRepoOwner       : headOwnerJsonElement.GetString (),
-                        Number              : numberJsonElement.GetInt32 (),
-                        HeadRef             : headRefJsonElement.GetString (),
-                        BaseRef             : baseRefJsonElement.GetString ()
-                    );
-                }
-
-                return null;
+                yield return PullRequest.Parse(je);
             }
         }
     }
 
-    public class JsonHelper
+    public static class JsonHelper
     {
-        public static bool TryGetElementByPath (JsonElement je, string path, out JsonElement resultJsonElement)
+        public static T Get<T> (this JsonElement je, string? path)
+        {
+            if (je.TryGet<T> (path, out var value))
+                return value;
+
+            throw new KeyNotFoundException($"Could not find '{je}' part of the path '{path}'");
+        }
+
+        public static bool TryGet<T> (this JsonElement je, string? path, [NotNullWhen(true)] out T value)
         {
             JsonElement current = je;
-            foreach (var pe in path.Split(new char[]{'/'}, StringSplitOptions.RemoveEmptyEntries)) {
-                if (current.ValueKind == JsonValueKind.Object && current.TryGetProperty (pe, out var propJsonElement)) {
-                    current = propJsonElement;
-                }
-                else {
-                    resultJsonElement = current;
-                    return false;
+            if (path != null) {
+                foreach (var pe in path.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries)) {
+                    if (current.ValueKind == JsonValueKind.Object && current.TryGetProperty(pe, out var propJsonElement)) {
+                        current = propJsonElement;
+                    } else {
+                        value = default(T)!;
+                        return false;
+                    }
                 }
             }
 
-            resultJsonElement = current;
+            if (typeof(T) == typeof(JsonElement)) {
+                value = (T)(object)current;
+            } else {
+                value = (T)Convert.ChangeType(current.GetRawText().Trim('"'), typeof(T));
+            }
+
             return true;
         }
     }
@@ -207,6 +263,28 @@ namespace EngUpdater
             this.Number = Number;
             this.HeadRef = HeadRef;
             this.BaseRef = BaseRef;
+        }
+
+        public static PullRequest? Parse (JsonElement je, bool verbose = false)
+        {
+            if (je.ValueKind != JsonValueKind.Object)
+                return null;
+
+            try
+            {
+                return new PullRequest(
+                   HTML_URL:        je.Get<string> ("html_url"),
+                   HeadRepoOwner:   je.Get<string> ("head/repo/owner/login"),
+                   Number:          je.Get<int>    ("number"),
+                   HeadRef:         je.Get<string> ("head/ref"),
+                   BaseRef:         je.Get<string> ("base/ref")
+                );
+            } catch (KeyNotFoundException knfe) {
+                if (verbose)
+                    Console.WriteLine ($"Error parsing PR : {knfe.Message}");
+            }
+
+            return null;
         }
     }
 
