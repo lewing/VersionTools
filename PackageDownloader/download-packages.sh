ed946bb69263f4b6e667d362a4d9971181b3f0e7
tqiu8 <tammy.qiu@yahoo.com>
2020-04-03 17:09:20 -0400

    set filepath to packages as variable

--- PackageDownloader/download-packages.sh
+++ PackageDownloader/download-packages.sh
@@ -26,21 +26,19 @@ PACKAGE_PATH=$HOME/.nuget/packages/microsoft.aspnetcore.components.webassembly.r
 
 if [ -z "$FILEPATH" ]
 then
-    echo "$URL"
     wget -O wasm-package.zip $URL
     unzip wasm-package.zip -d wasm-package/
-else
-    cd $FILEPATH
+    FILEPATH=wasm-package/
 fi
 
 rm -r $PACKAGE_PATH/bcl
-mv wasm-package/wasm-bcl $PACKAGE_PATH/bcl
+mv $FILEPATH/wasm-bcl $PACKAGE_PATH/bcl
 
 rm -r $PACKAGE_PATH/framework
-mv wasm-package/framework $PACKAGE_PATH/framework
+mv $FILEPATH/framework $PACKAGE_PATH/framework
 
 rm "$PACKAGE_PATH"/wasm/*
-mv wasm-package/builds/release/dotnet.* $PACKAGE_PATH/wasm/
+mv $FILEPATH/builds/release/dotnet.* $PACKAGE_PATH/wasm/
 
 rm wasm-package.zip
-rm -r wasm-package
+rm -rf wasm-package
